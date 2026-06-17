71e27a8873b52370b009742eeb0d29a0d8a167ef
Ants-saunamees <sander.rammer77@gmail.com>
2026-06-17 15:15:21 +0300

    Fixing ingestion

--- config/chroma_db.py
+++ config/chroma_db.py
@@ -2,8 +2,11 @@ import chromadb
 from sentence_transformers import SentenceTransformer
 import uuid
 
-CHROMA_PATH = "./chroma_db"
 EMBED_MODEL = "all-MiniLM-L6-v2"
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CHROMA_PATH = os.path.join(BASE_DIR, "chroma_db")
 
 chroma_client = chromadb.PersistentClient(path=CHROMA_PATH)
 
@@ -17,6 +20,7 @@ embedder = SentenceTransformer(EMBED_MODEL)
 
 
 def store_vector(doc_id: str, embedding: list[float], text: str, metadata: dict):
+    print("Store vector:", doc_id)
     collection.upsert(
         ids=[doc_id],
         embeddings=[embedding],
@@ -34,3 +38,11 @@ def search_vectors(query: str, k: int = 5):
     )
 
     return results["documents"][0]
+
+
+def count_vectors():
+    return collection.count()
+
+
+if __name__ == "__main__":
+    print("TOTAL VECTORS:", count_vectors())
--- ingestion/ingestion_pipeline.py
+++ ingestion/ingestion_pipeline.py
@@ -1,25 +1,21 @@
 from ingestion.loaders import loader
 from ingestion.chunker import chunker
 from llm.embedder import embed_text
-from config.chroma_db import store_vector
-import asyncio
+from config.chroma_db import store_vector, collection
 import uuid
+import asyncio
 
 async def pipeline(department: str):
-    for result in loader(department):   # iterate the generator
-        print("1")
-
-        # no index creation needed for Chroma
-        print("2")
-
+    for result in loader(department):
         for chunk in chunker(result["text"], result["metadata"]["department"]):
-            id = str(uuid.uuid4())
-            print("3")
-
+            doc_id = str(uuid.uuid4())
             embedding = await embed_text(chunk)
-            print("4")
+            store_vector(doc_id, embedding, chunk, result["metadata"]) # prg peab koik reingestima kui addid juurde
 
-            store_vector(id, embedding, chunk, result["metadata"])
+    # AFTER ingestion is done
+    print("Flushing DB...")
+    print("Final count:", collection.count())
+
 
 if __name__ == "__main__":
     asyncio.run(pipeline("pets"))
